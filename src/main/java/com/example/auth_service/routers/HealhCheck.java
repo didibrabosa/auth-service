62e975e7cdcda6e3835e5010d7e9ff8da220eff1
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-04-16 19:53:53 -0300

    BIG BANG

---
+++ src/main/java/com/example/auth_service/routers/HealhCheck.java
@@ -0,0 +1,15 @@
+package com.example.auth_service.routers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class HealhCheck {
+    
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "API is up and health!";
+    }
+}
