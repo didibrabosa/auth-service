ab78d84836b37cee18376f0bf2ebebf9d884ebaf
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-04-22 21:14:47 -0300

    chore: improving docker and putting the container on a network to communicate with another service

--- src/main/java/com/example/auth_service/routers/HealthCheck.java
+++ src/main/java/com/example/auth_service/routers/HealthCheck.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HealhCheck {
+public class HealthCheck {
     
     @GetMapping("/health")
     public String healthCheck() {
